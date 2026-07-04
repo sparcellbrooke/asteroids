f0848eff698a32bee756e2d71834542429bd2c68
sparcellbrooke <sparcellbrooke@gmail.com>
2026-07-04 14:19:38 +1000

    CH3: Player L3: Moving Around

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
             pass
 
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()  
         dt = clock.tick(60) / 1000    
