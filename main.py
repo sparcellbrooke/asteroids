9740d5103b9a4defc8fda98d92abb3d978fc769b
sparcellbrooke <sparcellbrooke@gmail.com>
2026-07-04 13:45:23 +1000

    CH2: Gameloop complete

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+   
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock =  pygame.time.Clock()
+    dt = 0.0
 
-
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()  
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
